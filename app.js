2b9281c0cf795529b6f032f353893f516c9298df
Crawdon <67289352+Crawdon@users.noreply.github.com>
2021-07-17 19:58:59 -0300

    added styles

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+
 Vue.component("card", {
-    props: ["title", "pContent"],
+    props: {
+        title:String,
+        body:String
+    },
     template: `
-      <div>
-      <h2>{{ title }}</h2>  
-      <p>{{ pContent }}</p>
+      <div class="card">
+      <h2 class="card-title">{{ title }}</h2>  
+      <p class="card-body">{{ body }}</p>
       </div>
     `,
   });
@@ -13,47 +17,28 @@ var app = new Vue({
     data: {   
         cards:[
             {
-            title:"Titulo 1 configurable",
-            pContent:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            title:"Titulo 1",
+            body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
             },
             {
-            title:"Titulo 2 configurable",
-            pContent:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            title:"Titulo 2",
+            body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
             },
             {
-            title:"Titulo 3 configurable",
-            pContent:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            title:"Titulo 3",
+            body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
             }
         ]     
     },
     template:
-        `
-        <div>
-            <card 
-                v-for="(card,index) in cards"
-                :key="index"
-                :title="card.title"
-                :pContent="card.pContent"
-                >
-            </card>
-        </div>`
-  })
-
-
-/* 
-
--font family arial
--titulo configurable
--backgroundcolor #ccc
--p font size 14px
-wxh 150*300 px
-
-
-Card.init({
-el: selector,
-config: {
-
-title: '',
-body: ''
-}
-}); */
+    `
+    <div>
+        <card 
+            v-for="(card,index) in cards"
+            :key="index"
+            :title="card.title"
+            :body="card.body"
+            >
+        </card>
+    </div>`
+  }) 
